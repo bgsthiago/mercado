fc9877a89bfbcdce3367be8c26f2256485e3a40f
chadychaito <chadychaito@gmail.com>
2016-04-26 19:24:55 -0300

    Commit final

--- code/listaCompras.c
+++ code/listaCompras.c
@@ -1,5 +1,5 @@
 #include "listaCompras.h"
-#include "estrutura.h"
+#include "estoque.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -13,47 +13,6 @@ int criarListaDeCompra(LISTA_DE_COMPRA *lc){
   lc->inicio == NULL;
 return 1;
 }
-
-// Retorna 1 se deu certo e 0 caso contrario
-/*int inserirCompra(LISTA_DE_COMPRA *lc, char *nomeProd, int quantidade, ESTOQUE *e){
-  NO_LISTA *novo = (NO_LISTA) malloc(sizeof(NO_LISTA));
-  NO_LISTA *aux = lc->inicio;
-  count = 0;
-  novo->prod.nomeProd = nomeProd;
-  novo->prox = NULL;
-
-  if(estoqueVazio(e)) return 0;
-  while((strcmp(nomeProd, e->item[count].nomeProd) != 0) && count<=MAX) count++; //Proucurando o produto no Estoque por nome
-  if (count > MAX) return 0; //Nao existe este item no Estoque
-
-  //Achou item no estoque
-  if(e->item[count].qtdd < quantidade){
-    novo->prod.qtdd = e->item[count].qtdd;
-
-    //COMO TIRAR A STRUCT E SUBSTITUIR POR 0.
-    //e->item[count] = 0; //Removendo do Estoque colocando o simbolo 0 como ref.
-  }
-  else{
-    novo->prod.qtdd = quantidade;
-    e->item[count].qtdd -= quantidade;
-  }
-  novo->prod.preco = (e->item[count].preco)*(novo->prod.qtdd);
-
-  if(lc->inicio == NULL){
-    lc->inicio = novo;
-    lc->quantidade_itens = novo->prod.qtdd;
-    lc->total_preco = novo->prod.preco;
-  }
-  else{
-    while(aux->prox != NULL){
-      aux = aux->prox;
-    }
-    aux->prox = novo;
-    lc->quantidade_itens += novo->prod.qtdd;
-    lc->total_preco += novo->prod.preco;
-  }
-}
-*/
 
 // Retorna 1 se deu certo e 0 caso contrario
 int inserirCompra(LISTA_DE_COMPRA *lc, char *nomeProd, int quantidade, ESTOQUE *e){
@@ -70,7 +29,6 @@ int inserirCompra(LISTA_DE_COMPRA *lc, char *nomeProd, int quantidade, ESTOQUE *
     novo->prod.qtdd = quantidade
   else
     novo->prod.qtdd = e->item[contador].qtdd;
-
   novo->prod.preco = e->item[contador].preco;
   novo->prox = NULL;
 
@@ -86,8 +44,8 @@ int inserirCompra(LISTA_DE_COMPRA *lc, char *nomeProd, int quantidade, ESTOQUE *
      novo->prox = aux;
      lc->inicio = novo;
      removerDoEstoque(e, novo->prod);
-   }
-   else{
+    }
+    else{
      while((strcmp(atual->prod.nomeProd, nomeProd) < 0) && atual != NULL){
        anterior = atual;
        atual = atual->prox;
@@ -109,6 +67,8 @@ int inserirCompra(LISTA_DE_COMPRA *lc, char *nomeProd, int quantidade, ESTOQUE *
      }
    }
  }
+ lc->quantidade_items += novo->prod.qtdd;
+ lc->total_preco += (novo->prod.preco) * (novo->prod.qtdd);
 }
 
 // Retorna 1 se deu certo e 0 caso contrario
